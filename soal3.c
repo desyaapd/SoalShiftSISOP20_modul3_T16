aeb9a5631383098002d664679abad712d8e23919
desyaapd <49356926+desyaapd@users.noreply.github.com>
2020-04-03 17:34:57 +0700

    Create soal3.c

---
+++ soal3.c
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include<sys/wait.h>
