3d7d53de709a6333af37a6ca343ce954eddb3246
desyaapd <49356926+desyaapd@users.noreply.github.com>
2020-04-11 21:43:07 +0700

    Add files via upload

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -7,4 +7,169 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include<sys/wait.h>
+#include <dirent.h>
+
+char *getNamaFile(char *fName, char buff[]) {
+  char *token = strtok(fName, "/");
+  while (token != NULL) {
+    sprintf(buff, "%s", token);
+    token = strtok(NULL, "/");
+  }
+}
+
+char *getExtension(char *fName, char buff[]) {
+  char buffNamaFile[1337];
+  char *token = strtok(fName, "/");
+  while (token != NULL) {
+    sprintf(buffNamaFile, "%s", token);
+    token = strtok(NULL, "/");
+  }
+  int count = 0;
+  token = strtok(buffNamaFile, ".");
+  while(token != NULL) {
+    count++;
+    sprintf(buff, "%s", token);
+    token = strtok(NULL, ".");
+  }
+  if (count <= 1) {
+    strcpy(buff, "unknown");
+  }
+
+  return buff;
+}
+
+void dirChecking(char buff[]) {
+  DIR *dr = opendir(buff);
+  if (ENOENT == errno) {
+    mkdir(buff, 0775);
+    closedir(dr);
+  }
+}
+
+struct args {
+  char *buffer;
+};
+
+void *routine(void* arg) {
+  char buffExt[100];
+  char buffFileName[1337];
+  char buffFrom[1337];
+  char buffTo[1337];
+  char cwd[1337];
+  getcwd(cwd, sizeof(cwd));
+  strcpy(buffFrom, (char *) arg);
+
+  if (access(buffFrom, F_OK) == -1) {
+    printf("File %s tidak ada\n", buffFrom);
+    pthread_exit(0);
+  }
+  DIR* dir = opendir(buffFrom);
+  if (dir) {
+    printf("file %s berupa folder\n", buffFrom);
+    pthread_exit(0);
+  }
+  closedir(dir);
+
+  getNamaFile(buffFrom, buffNamaFile);
+  strcpy(buffFrom, (char *) arg);
+
+  getExtension(buffFrom, buffExt);
+  for (int i = 0; i < sizeof(buffExt); i++) {
+    buffExt[i] = tolower(buffExt[i]);
+  }
+  strcpy(buffFrom, (char *) arg);
+
+  dirChecking(buffExt);
+
+  sprintf(buffTo, "%s/%s/%s", cwd, buffExt, buffNamaFile);
+  rename(buffFrom, buffTo);
+
+  pthread_exit(0);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    printf("Argument kurang\n");
+    exit(1);
+  }
+  if (strcmp(argv[1], "-f") != 0 && strcmp(argv[1], "*") != 0 && strcmp(argv[1], "-d")) {
+    printf("Argument tidak ada\n");
+    exit(1);
+  }
+
+  if (strcmp(argv[1], "-f") == 0) {
+    if (argc <= 2) {
+      printf("Argument salah\n");
+      exit(1);
+    }
+
+    pthread_t tid[argc-2];
+    for (int i = 2; i < argc; i++) {
+      pthread_create(&tid[i-2], NULL, &routine, (void *)argv[i]);
+    }
+    for (int i = 2; i < argc; i++) {
+      pthread_join(tid[i-2], NULL);
+    }
+    exit(0);
+  }
+
+  char *directory;
+  if (strcmp(argv[1], "*") == 0) {
+    if (argc != 2) {
+      printf("Argument salah\n");
+      exit(1);
+    }
+    char buff[1337];
+    getcwd(buff, sizeof(buff));
+    directory = buff;
+  }
+
+  if (strcmp(argv[1], "-d") == 0) {
+    if (argc != 3) {
+      printf("Argument salah\n");
+      exit(1);
+    }
+    DIR* dir = opendir(argv[2]);
+    if (dir) {
+      directory = argv[2];
+    } else if (ENOENT == errno) {
+      printf("Directory tidak ada\n");
+      exit(1);
+    }
+    closedir(dir);
+  }
+
+  int file_count = 0;
+  DIR* dir = opendir(directory);
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_REG) {
+      file_count++;
+    }
+  }
+  closedir(dir);
+
+  pthread_t tid[file_count];
+  char buff[file_count][1337];
+  int iter = 0;
+
+  dir = opendir(directory);
+  while ((entry = readdir(dir)) != NULL) {
+    if (entry->d_type == DT_REG) {
+      sprintf(buff[iter], "%s/%s", directory, entry->d_name);
+      iter++;
+    }
+  }
+  closedir(dir);
+
+  for (int i = 0; i < file_count; i++) {
+    char  *test = (char*)buff[i];
+    printf("%s\n", test);
+    pthread_create(&tid[i], NULL, &routine, (void *)test);
+  }
+
+  for (int i = 0; i < file_count; i++) {
+    pthread_join(tid[i], NULL);
+  }
+
+}
